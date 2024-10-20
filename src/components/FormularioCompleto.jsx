f6aab9eb5ffa4e052dcf011baffa33294cd3a0ee
Neilon <neilon@ispcloud.com.br>
2024-10-20 20:19:52 -0300

    ajustes resposividade

--- src/components/FormularioCompleto.jsx
+++ src/components/FormularioCompleto.jsx
@@ -1,10 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Input from "./Input";
 import InputMascara from "./InputMascara";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import validator from "validator";
+import { ClipLoader } from "react-spinners";
+
+const LoadingSpinner = () => {
+  return <ClipLoader color="#ffffff" size={20} />;
+};
 
 const cleanData = (input) => {
   return input.replace(/\D/g, ""); // Remove tudo que não é número
@@ -27,11 +32,6 @@ function FormularioCompleto() {
   const [uf, setUF] = useState("");
   const [loading, setLoading] = useState(false);
   const [loadingEnvio, setLoadingEnvio] = useState(false);
-
-  // Função para alternar entre CPF e CNPJ
-  const toggleTipoDocumento = () => {
-    setTipoDocumento((prev) => (prev === "CPF" ? "CNPJ" : "CPF"));
-  };
 
   // Função para consultar o CEP
   const consultarCEP = () => {
@@ -85,6 +85,28 @@ function FormularioCompleto() {
 
     if (!telefone || !documento || !nome || !cep) {
       toast.error("É preciso preencher todos os campos");
+      setLoadingEnvio(false);
+      return;
+    }
+
+    if (!validator.isEmail(email)) {
+      toast.error("Email inválido");
+      setLoadingEnvio(false);
+      return;
+    }
+
+    if (telefone.length < 11) {
+      toast.error("Telefone inválido");
+      setLoadingEnvio(false);
+      return;
+    }
+
+    if (tipoDocumento === "CPF" && cleanData(documento).length !== 11) {
+      toast.error("CPF inválido");
+      setLoadingEnvio(false);
+      return;
+    } else if (tipoDocumento === "CNPJ" && cleanData(documento).length !== 14) {
+      toast.error("CNPJ inválido");
       setLoadingEnvio(false);
       return;
     }
@@ -180,10 +202,12 @@ function FormularioCompleto() {
         className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
       />
 
-      <div className="space-x-3">
+      <div className="flex space-x-3 mb-4">
         <button
-          onClick={toggleTipoDocumento}
-          className="mb-2 p-2 px-6 w-[83px] bg-slate-400 hover:bg-slate-500 text-white rounded-md font-medium"
+          onClick={() =>
+            setTipoDocumento(tipoDocumento === "CPF" ? "CNPJ" : "CPF")
+          }
+          className="p-2 px-6 bg-slate-400 hover:bg-slate-500 text-white rounded-md font-medium"
         >
           {tipoDocumento === "CPF" ? "CPF" : "CNPJ"}
         </button>
@@ -193,17 +217,20 @@ function FormularioCompleto() {
           placeholder="Documento"
           value={documento}
           onChange={(event) => setDocumento(event.target.value)}
-          className="border border-slate-300 outline-slate-400 py-2 rounded-md px-4 required w-[220px]"
+          className="border border-slate-300 outline-slate-400 py-2 rounded-md px-4 w-full"
           mask={
             tipoDocumento === "CPF" ? "999.999.999-99" : "99.999.999/9999-99"
           }
         />
+      </div>
+
+      <div className="flex space-x-3 mb-4">
         <InputMascara
           type="text"
           placeholder="Telefone"
           value={telefone}
           onChange={(event) => setTelefone(event.target.value)}
-          className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md required w-[220px] mb-10"
+          className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md w-full"
           mask="(99) 99999-9999"
         />
 
@@ -212,108 +239,94 @@ function FormularioCompleto() {
           placeholder="Email"
           value={email}
           onChange={(event) => setEmail(event.target.value)}
-          className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md required w-[290px] mb-10"
+          className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md w-full"
         />
       </div>
 
-      {/* Inputs de endereço */}
-      <div className="space-y-4 bg-slate-200 rounded-md flex flex-col mt-15">
-        <div className="space-x-3">
-          <InputMascara
-            type="text"
-            placeholder="CEP"
-            value={cep}
-            onChange={(event) => setCEP(event.target.value)}
-            className="border border-slate-300 outline-slate-400 py-2 rounded-md px-4 required w-96"
-            mask="99999-999"
-          />
+      {/* Alinhamento do botão com o campo CEP */}
+      <div className="flex space-x-3 mb-4">
+        <InputMascara
+          type="text"
+          placeholder="CEP"
+          value={cep}
+          onChange={(event) => setCEP(event.target.value)}
+          className="border border-slate-300 outline-slate-400 py-2 rounded-md px-4 required w-full"
+          mask="99999-999"
+        />
 
-          <button
-            onClick={consultarCEP}
-            disabled={loading}
-            type="button"
-            className={`text-white ${
-              loading ? "bg-slate-400" : "bg-slate-400 hover:bg-slate-500"
-            } focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 inline-flex items-center`}
-          >
-            {loading && <LoadingSpinner />}
-            {!loading ? "Consultar" : "Aguarde ..."}
-          </button>
-        </div>
+        <button
+          onClick={consultarCEP}
+          disabled={loading}
+          type="button"
+          className={`text-white ${
+            loading ? "bg-slate-400" : "bg-slate-400 hover:bg-slate-500"
+          } focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center justify-center min-w-[150px]`} // Ajuste o valor de min-w conforme necessário
+        >
+          {loading && <LoadingSpinner />}
+          {!loading ? "Consultar" : ""}
+        </button>
+      </div>
 
+      {/* Endereço e Cidade (70% / 30% para responsividade) */}
+      <div className="grid grid-cols-1 mb-4">
         <Input
           type="text"
           placeholder="Endereço"
-          value={endereco}
-          className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+          value={uf}
           onChange={(event) => setEndereco(event.target.value)}
+          className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
         />
-        <div className="space-x-4">
-          <Input
-            type="text"
-            placeholder="Cidade"
-            value={cidade}
-            className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md w-[450px]"
-            onChange={(event) => setCidade(event.target.value)}
-          />
+      </div>
 
-          <Input
-            type="text"
-            placeholder="Bairro"
-            value={bairro}
-            className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md w-[180px]"
-            onChange={(event) => setBairro(event.target.value)}
-          />
-          <Input
-            type="text"
-            placeholder="Número"
-            value={numero}
-            className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md w-[100px]"
-            onChange={(event) => setNumero(event.target.value)}
-          />
-          <Input
-            type="text"
-            placeholder="UF"
-            value={uf}
-            className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md w-[70px]"
-            onChange={(event) => setUF(event.target.value)}
-          />
-        </div>
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <Input
+          type="text"
+          placeholder="Bairro"
+          value={bairro}
+          onChange={(event) => setBairro(event.target.value)}
+          className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        />
+
+        <Input
+          type="text"
+          placeholder="Número"
+          value={numero}
+          onChange={(event) => setNumero(event.target.value)}
+          className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <Input
+          type="text"
+          placeholder="Cidade"
+          value={cidade}
+          onChange={(event) => setCidade(event.target.value)}
+          className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        />
+        <Input
+          type="text"
+          placeholder="Estado"
+          value={endereco}
+          onChange={(event) => setUF(event.target.value)}
+          className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        />
       </div>
 
       <button
         onClick={handleSubmit}
         disabled={loadingEnvio}
-        className={`text-white px-4 py-2 rounded-md font-medium w-full flex justify-center items-center ${
-          loadingEnvio ? "bg-slate-400" : "bg-slate-400 hover:bg-slate-500"
-        } focus:ring-4 focus:ring-gray-200`}
+        type="button"
+        className={`bg-slate-400 hover:bg-slate-500 text-white font-medium rounded-lg text-sm px-5 py-2.5 relative flex items-center justify-center`}
       >
-        {loadingEnvio && <LoadingSpinner />}
-        {!loadingEnvio ? "Enviar" : "Aguarde ..."}
+        {loadingEnvio && (
+          <LoadingSpinner className="absolute left-1/2 transform -translate-x-1/2" />
+        )}
+        {!loadingEnvio ? "Enviar" : ""}
       </button>
+
       <ToastContainer />
     </div>
   );
 }
-
-const LoadingSpinner = () => (
-  <svg
-    aria-hidden="true"
-    role="status"
-    className="inline w-4 h-4 me-3 text-white animate-spin"
-    viewBox="0 0 100 101"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-      fill="#E5E7EB"
-    />
-    <path
-      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-      fill="currentColor"
-    />
-  </svg>
-);
 
 export default FormularioCompleto;
